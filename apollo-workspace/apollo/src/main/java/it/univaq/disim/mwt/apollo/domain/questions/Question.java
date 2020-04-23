6ab696a0c5ed40a54540de55e8e72b1d62db9c53
Davide <dav.romano@icloud.com>
2020-04-23 12:46:44 +0200

    adding indexed annotation on urlId

--- apollo-workspace/apollo/src/main/java/it/univaq/disim/mwt/apollo/domain/questions/Question.java
+++ apollo-workspace/apollo/src/main/java/it/univaq/disim/mwt/apollo/domain/questions/Question.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
