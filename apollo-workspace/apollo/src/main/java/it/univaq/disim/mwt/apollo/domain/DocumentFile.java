75520a0f14aba2bad50fc8232312b77029d2cc73
Giordano <d'Aloisio>
2020-06-04 18:47:46 +0200

    fix text question and add radio question submit

--- apollo-workspace/apollo/src/main/java/it/univaq/disim/mwt/apollo/domain/DocumentFile.java
+++ apollo-workspace/apollo/src/main/java/it/univaq/disim/mwt/apollo/domain/DocumentFile.java
@@ -1,5 +1,7 @@
 package it.univaq.disim.mwt.apollo.domain;
 
+import java.util.Base64;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -8,7 +10,9 @@ import org.bson.types.Binary;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Document(collection = "SurveyFiles")
@@ -20,5 +24,11 @@ public class DocumentFile {
 	private String id;
 	
 	private String name;
+	
+	@Getter(AccessLevel.NONE)
 	private Binary data;
+	
+	public String getData() {
+		return Base64.getEncoder().encodeToString(data.getData());
+	}
 }
