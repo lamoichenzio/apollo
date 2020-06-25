b39c96286c4af66d48df44841431abc77078d962
Giordano <d'Aloisio>
2020-06-25 10:40:45 +0200

    Revert "Merge remote-tracking branch 'origin/Layouting' into develop"
    This reverts commit e2e3cedcc0dd28236145dff8d489cbc4781cbe3d.

--- apollo-workspace/apollo/src/main/resources/static/script/user_view.js
+++ apollo-workspace/apollo/src/main/resources/static/script/user_view.js
@@ -128,10 +128,3 @@ function surveyLogin(url, form){
         }
     })
 }
-
-/**
- * Set the value of other choice as the textbox input
- */
-function otherChoiceValueSetter(textbox) {
-    $("#" + textbox.name)[0].value = textbox.value;
-}
