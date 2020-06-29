f68a5e1c385006dfed4eeda0c3e81694f94a9468
GIordano <giordano.daloisio@gmail.com>
2020-06-29 18:46:27 +0200

    fixes

--- apollo-workspace/apollo/src/main/java/it/univaq/disim/mwt/apollo/presentation/GlobalExceptionHandler.java
+++ apollo-workspace/apollo/src/main/java/it/univaq/disim/mwt/apollo/presentation/GlobalExceptionHandler.java
@@ -20,29 +20,23 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler({NotFoundException.class, NoSuchElementException.class})
 	public String handleNotFoundException(HttpServletRequest request, Exception ex, Model model) {
-		log.info("[Exception Occured]:: URL=" + request.getRequestURL() + ", method=" + request.getMethod() + ", message=" + ex.getMessage());
-		log.info("[Exception Class Name]:: " + ex.getClass().getName());
-
-		StringWriter stringWriter = new StringWriter();
-		PrintWriter printWriter = new PrintWriter(stringWriter);
-		ex.printStackTrace(printWriter);
-		printWriter.flush();
-
+		handleException(request, ex);
 		return "/utility/error_404";
 	}
 
 	@ExceptionHandler({BusinessException.class, Exception.class})
 	public String handleBusinessException(HttpServletRequest request, Exception ex, Model model) {
+		handleException(request, ex);
+		return "/utility/error_500";
+	}
+
+	private void handleException(HttpServletRequest request, Exception ex) {
 		log.info("[Exception Occured]:: URL=" + request.getRequestURL() + ", method=" + request.getMethod() + ", message=" + ex.getMessage());
 		log.info("[Exception Class Name]:: " + ex.getClass().getName());
 
 		StringWriter stringWriter = new StringWriter();
 		PrintWriter printWriter = new PrintWriter(stringWriter);
-		
 		ex.printStackTrace(printWriter);
 		printWriter.flush();
-
-		return "/utility/error_500";
 	}
-	
 }
