f26dccbd0fa6b29efb647422e73c3ba73b74eb8c
Davide <dav.romano@icloud.com>
2020-07-07 20:27:35 +0200

    script and template refactoring

--- apollo-workspace/apollo/src/main/resources/static/script/user_script.js
+++ apollo-workspace/apollo/src/main/resources/static/script/user_script.js
@@ -1,4 +1,23 @@
-function displayalert(url){
+/**
+ * 
+ * User script file.
+ * 
+ */
+
+$(function () {
+    $("#file-1").change(() => {
+        let filename = $("#file-1").val();
+        $("#filename").html(filename.replace(/^.*\\/, ""));
+    });
+});
+
+/**
+ * Display alert.
+ * @param {String} url 
+ */
+function displayalert(url) {
+    console.info('[Alert]::[Open]');
+
     $.ajax({
         type: "GET",
         url: url,
@@ -11,28 +30,36 @@ function displayalert(url){
             $("#modal-delete-user").modal("show");
         },
         error: function (e) {
-            console.log('ERROR', e);
+            console.error('ERROR', e);
+            $('.alert-container').append(showAlert('error', 'Error!', e.responseJSON.msg)).show();
         }
     });
 }
 
+/**
+ * Handle submit
+ * @param {String} url 
+ * @param {String} redirectUrl 
+ */
 function submit(url, redirectUrl){
-    const pass = $("#password").val()
+    const pwd = $("#password").val();
+
     $.ajax({
         type: "POST",
         url: url,
-        data: pass,
+        data: pwd,
         dataType: 'json',
         contentType: 'application/json',
         cache: false,
         timeout: 10000,
         success: (response) => {
-            console.log(response);
+            console.info('[SUCCESS]::[Status]:', response.status);
             if(response.status === "OK"){
                 window.location.replace(redirectUrl);
             }
         },
         error: (response) => {
+            console.error('ERROR', e);
             if(response.responseJSON.status === "ERROR"){
                 $("#pass_error").show();
             }
